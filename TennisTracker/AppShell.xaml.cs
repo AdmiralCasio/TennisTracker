522adb1532313499c0518de8f212723c0c45b46f
lyonm <lyonm@MATTHEWS-PC>
2024-08-06 16:55:35 +0100

    Add project files.

---
+++ TennisTracker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TennisTracker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TennisTracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TennisTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
